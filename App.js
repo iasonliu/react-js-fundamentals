38128e0a43bc72f8829b12e05db00940899690ab
Liu Xiangxu <xiangxu.liu@cm-dt.com>
2016-09-12 11:30:44 +0800

    owner ownee relationship

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: '',
+      cat: 0
+    }
+    this.update = this.update.bind(this)
+  }
+  update(e){
+    this.setState({txt: e.target.value})
+  }
   render(){
-    let txt = this.props.txt
+    let txt = this.state.txt
     return (
       <div>
-        <h1>Hello World</h1>
-        <b>Blold</b>
-        <h1>{txt}</h1>
+        <Widget txt={txt} update={this.update}/>
+        <Widget txt={txt} update={this.update}/>
+        <Widget txt={txt} update={this.update}/>
+        <Widget txt={txt} update={this.update}/>
+        <Widget txt={txt} update={this.update}/>
       </div>
     );
   }
 }
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
+
+const Widget = (props) => {
+  return (
+    <div>
+    <h1>{props.txt}</h1>
+    <input type="text" onChange={props.update} />
+    </div>
+  )
 }
-App.defaultProps = {
-  txt: "this is the default txt"
-}
-ReactDOM.render(
-  <App cat={5} />,
-  document.getElementById('app')
-)
 //const App = () => <h1>Hello Eggheads</h1>
-// export default App
+export default App
