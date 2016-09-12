d8fe39f8b8505fa6d824424a26a0f7c3aa7f5593
Liu Xiangxu <xiangxu.liu@cm-dt.com>
2016-09-12 10:30:42 +0800

    the render method

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 class App extends React.Component {
   render(){
-    return(
-      <div><h1>Hello World</h1></div>
-    )
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Blold</b>
+      </div>
+    );
   }
 }
 
