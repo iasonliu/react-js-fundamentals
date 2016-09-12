82f010e6f360d91778919d7e1e89f15698bba5af
Liu Xiangxu <xiangxu.liu@cm-dt.com>
2016-09-12 11:46:56 +0800

    Refs to access components

--- App.js
+++ App.js
@@ -1,38 +1,50 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      txt: '',
-      cat: 0
+      red: 0,
+      green: 0,
+      blue: 0
+
     }
     this.update = this.update.bind(this)
   }
   update(e){
-    this.setState({txt: e.target.value})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    })
   }
   render(){
-    let txt = this.state.txt
     return (
       <div>
-        <Widget txt={txt} update={this.update}/>
-        <Widget txt={txt} update={this.update}/>
-        <Widget txt={txt} update={this.update}/>
-        <Widget txt={txt} update={this.update}/>
-        <Widget txt={txt} update={this.update}/>
+        <Slider ref="red" update={this.update}/>
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update}/>
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update}/>
+        {this.state.blue}
+        <br />
       </div>
     );
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-    <h1>{props.txt}</h1>
-    <input type="text" onChange={props.update} />
-    </div>
-  )
+class Slider extends React.Component {
+  render(){
+    return (
+      <div>
+      <input type="range" ref="inp" min="0" max="255" onChange={this.props.update} />
+      </div>
+    )
+  }
 }
+
 //const App = () => <h1>Hello Eggheads</h1>
 export default App
