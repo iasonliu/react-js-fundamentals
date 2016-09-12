19de1413134164172ba8c123de1d8bc8391e3eed
Liu Xiangxu <xiangxu.liu@cm-dt.com>
2016-09-12 10:28:12 +0800

    hello world

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 class App extends React.Component {
   render(){
-    return <div>Hello</div>
+    return(
+      <div><h1>Hello World</h1></div>
+    )
   }
 }
 
+
+//const App = () => <h1>Hello Eggheads</h1>
 export default App
